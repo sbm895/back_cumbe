2342c154a09aa479ca483283def7f27240300589
Sebastian Barreto <sebmr895@gmail.com>
2026-05-06 21:01:37 -0500

    mistake

--- services/event-service/src/models.py
+++ services/event-service/src/models.py
@@ -6,6 +6,7 @@ from pydantic import EmailStr
 class User(Document):
     name: str
     email: EmailStr
+    fcm_token: Optional[str] = None  # se actualiza cada login
     profile_picture: Optional[str] = None      # URL o path de la foto
     favorites: list[str] = []                   # IDs de eventos favoritos
     attended_events: list[str] = []             # IDs de eventos asistidos
@@ -23,6 +24,6 @@ class Event(Document):
     organizer: User
     attendees: list[str] = []    # IDs de usuarios asistentes
     categories: list[str] = []   # Categorías del evento
-    
+
     class Settings:
         name = "events"
